9e13cb823d7bea987ec5037fdc121e530b705ace
vitoria.sousa <maria.v.cosmo@gmail.com>
2025-02-23 22:46:14 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("Bem vindo ", name);
+  }
+  return (
+    <div>
+      <h1>Form + Action</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
